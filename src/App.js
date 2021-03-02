3add2d9ce0ce7263af99cdf8cd1adea1a4d0edec
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-03-01 22:13:18 -0300

    added handling enter method in UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery';
@@ -10,7 +9,8 @@ class App extends Component {
     manager: '',
     players: [],
     balance: '',
-    value: ''
+    value: '',
+    message: ''
   };
 
   async componentDidMount(){
@@ -19,6 +19,21 @@ class App extends Component {
     const balance = await web3.eth.getBalance(lottery.options.address);
 
     this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({message: 'You have been entered!'});
   }
 
   render() {
@@ -30,7 +45,7 @@ class App extends Component {
           There are currently {this.state.players.length} people entered, competing to win { web3.utils.fromWei(this.state.balance, 'ether') } ether!
         </p>
         <hr />
-        <form>
+        <form onSubmit={this.onSubmit}>
             <h4>Want to try your luck?</h4>
             <div>
               <label>Amount of ether to enter</label>
@@ -40,6 +55,10 @@ class App extends Component {
             </div>
             <button>Enter</button>
         </form>
+
+        <hr/>
+
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
