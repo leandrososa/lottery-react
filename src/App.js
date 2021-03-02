d4e5767e8165ac33685707fe2355478858554d51
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-03-01 22:30:27 -0300

    added pick winner method in UI

--- src/App.js
+++ src/App.js
@@ -36,6 +36,18 @@ class App extends Component {
     this.setState({message: 'You have been entered!'});
   }
 
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({message: 'A winner has been picked!'});
+  }
+
   render() {
     return (
       <div>
@@ -55,9 +67,11 @@ class App extends Component {
             </div>
             <button>Enter</button>
         </form>
+        <hr/>
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
 
         <hr/>
-
         <h1>{this.state.message}</h1>
       </div>
     );
